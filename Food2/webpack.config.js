f8f96d99ce07736b72a59450f2dbaa1962382aad
Alex <pileroid@gmail.com>
2021-11-05 05:55:06 +0300

    final

--- Food2/webpack.config.js
+++ Food2/webpack.config.js
@@ -3,13 +3,32 @@
 let path = require('path');
 
 module.exports = {
-    mode: 'development',
-    entry: './js/script.js',
-    output: {
-        filename: 'bundle.js',
-        path: __dirname + '/dist/js'
-    },
-    watch: true,
-    devtool: 'source-map',
-    module: {},
-};0
+  mode: 'production',
+  entry: './js/script.js',
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/js'
+  },
+  watch: true,
+
+  devtool: "source-map",
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', {
+                debug: true,
+                corejs: 3,
+                useBuiltIns: "usage"
+            }]]
+          }
+        }
+      }
+    ]
+  }
+};
